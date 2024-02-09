59b6b889c8a92fd6a79ec785b832294bcca84afd
Quentin Horgues <quentin.horgues@outlook.fr>
2024-02-09 22:41:45 +0100

    add test

--- test/main.cpp
+++ test/main.cpp
@@ -13,10 +13,16 @@ int main()
                 {3},
                 {2}
         };
+
+        constexpr Matrix<int , 4> A {
+                Matrix<int, 2>{}          , Matrix<int, 2>{{1, 1}, {2, 3}},
+                Matrix<int, 2>::identity(), Matrix<int, 2>{}
+        };
      
                 
         constexpr Matrix mat { mat2 * mat1 };
 
         std::cout << mat1 << " *\n" << mat2 << " =\n" << mat << std::endl;
+        std::cout << A << std::endl;
 
 }
